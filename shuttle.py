cf68ff757dce3869f43135243b3b033a99a39c7e
xienina <xienina1985@163.com>
2017-01-06 21:02:02 +0800

    Update shuttle.py

--- shuttle.py
+++ shuttle.py
@@ -4,22 +4,11 @@ import sys
 import os
 import time
 
-import os 
-import time 
-import ntplib 
-c = ntplib.NTPClient() 
-response = c.request('pool.ntp.org') 
-ts = response.tx_time 
-_date = time.strftime('%Y-%m-%d',time.localtime(ts)) 
-_time = time.strftime('%X',time.localtime(ts)) 
-this_time = os.system('date {} && time {}'.format(_date,_time))
-
 TIMEFMT = "%Y-%m-%d %H:%M:%S"
-
 #
 ## Line 1 XiErQi Station(Line 13) - Incubator - ISB - Oracle Building
 ##
-## Morning Runs	7:40am - 9:30am
+## Morning Runs 7:40am - 9:30am
 ##     Capacity
 ##       4 buses, 50 seats for each
 ##     Route
@@ -29,7 +18,7 @@ TIMEFMT = "%Y-%m-%d %H:%M:%S"
 ##       8:40am/8:50am/9:00am/9:10am/9:20am/9:30am
 ##       1 bus at each departure time
 ##
-## Evening Runs 	17:40pm - 20:00pm
+## Evening Runs         17:40pm - 20:00pm
 ##     Route
 ##       Oracle Building -> ISB -> Incubator -> XiErQi Station(Line 13)
 ##     Departure Time
@@ -54,127 +43,127 @@ TT = ["07:40,A", "07:50,A", "08:00,A", "08:10,A",
 # Public functions to print out colorful string on terminal
 #
 def isatty():
-	s = os.getenv("ISATTY")
-	if s == None:
-		s = ""
+        s = os.getenv("ISATTY")
+        if s == None:
+                s = ""
 
-	if s.upper() == "YES":
-		return (True)
+        if s.upper() == "YES":
+                return (True)
 
-	if s.upper() == "NO":
-		return (False)
+        if s.upper() == "NO":
+                return (False)
 
-	if sys.stdout.isatty() and sys.stderr.isatty():
-		return (True)
-	return (False)
+        if sys.stdout.isatty() and sys.stderr.isatty():
+                return (True)
+        return (False)
 
 def str2gray(s):
-	if isatty():
-		return ("\033[1;30m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;30m%s\033[m" % s)
+        return (s)
 
 def str2red(s):
-	if isatty():
-		return ("\033[1;31m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;31m%s\033[m" % s)
+        return (s)
 
 def str2green(s):
-	if isatty():
-		return ("\033[1;32m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;32m%s\033[m" % s)
+        return (s)
 
 def str2yellow(s):
-	if isatty():
-		return ("\033[1;33m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;33m%s\033[m" % s)
+        return (s)
 
 def str2blue(s):
-	if isatty():
-		return ("\033[1;34m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;34m%s\033[m" % s)
+        return (s)
 
 def str2magenta(s):
-	if isatty():
-		return ("\033[1;35m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;35m%s\033[m" % s)
+        return (s)
 
 def str2cyan(s):
-	if isatty():
-		return ("\033[1;36m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;36m%s\033[m" % s)
+        return (s)
 
 def str2white(s):
-	if isatty():
-		return ("\033[1;37m%s\033[m" % s)
-	return (s)
+        if isatty():
+                return ("\033[1;37m%s\033[m" % s)
+        return (s)
 
 def getseconds(dayfrom, dayto):
-	tfa = time.strptime(dayfrom, TIMEFMT)
-	tfs = time.mktime(tfa)
-	tta = time.strptime(dayto, TIMEFMT)
-	tts = time.mktime(tta)
-	nsecs = tts - tfs
-	return (nsecs)
+        tfa = time.strptime(dayfrom, TIMEFMT)
+        tfs = time.mktime(tfa)
+        tta = time.strptime(dayto, TIMEFMT)
+        tts = time.mktime(tta)
+        nsecs = tts - tfs
+        return (nsecs)
 
 def get_minsecs(l):
-	#this_time = time.strftime(TIMEFMT, time.localtime())
-	prefix = this_time.split(' ')[0]
+        this_time = time.strftime(TIMEFMT, time.localtime())
+        prefix = this_time.split(' ')[0]
 
-	that_time = ""
-	that_type = ""
-	target = 24 * 60 * 60
-	for i in l:
-		i_list = i.split(',')
-		i_time = i_list[0]
-		i_type = i_list[1]
-		t = "%s %s:00" % (prefix, i_time)
-		n = getseconds(this_time, t)
-		#print "this: %s, that: %s, nsecs=%d" % (this_time, t , n)
-		if n <= 0:
-			continue
+        that_time = ""
+        that_type = ""
+        target = 24 * 60 * 60
+        for i in l:
+                i_list = i.split(',')
+                i_time = i_list[0]
+                i_type = i_list[1]
+                t = "%s %s:00" % (prefix, i_time)
+                n = getseconds(this_time, t)
+                #print "this: %s, that: %s, nsecs=%d" % (this_time, t , n)
+                if n <= 0:
+                        continue
 
-		if n < target:
-			target = n
-			that_time = i_time
-			that_type = i_type
-			#print "X: %d %s" % (n, i)
-	if that_type == 'A':
-		msg = A
-	elif that_type == "B":
-		msg = B
-	elif that_type == "C":
-		msg = C
-	else:
-		msg = ""
+                if n < target:
+                        target = n
+                        that_time = i_time
+                        that_type = i_type
+                        #print "X: %d %s" % (n, i)
+        if that_type == 'A':
+                msg = A
+        elif that_type == "B":
+                msg = B
+        elif that_type == "C":
+                msg = C
+        else:
+                msg = ""
 
-	d    = int(target / (24 * 3600))
-	left = target - d * 24 * 3600
-	h    = int(left / 3600)
-	left = left - h * 3600
-	m    = int(left / 60)
-	left = left - m * 60
-	s    = int(left)
+        d    = int(target / (24 * 3600))
+        left = target - d * 24 * 3600
+        h    = int(left / 3600)
+        left = left - h * 3600
+        m    = int(left / 60)
+        left = left - m * 60
+        s    = int(left)
 
-	if len(that_time) > 0:
-		s1 = str2red("Next bus time:")
-		s2 = str2yellow(that_time)
-		s3 = str2red("Time left:")
-		s4 = str2yellow("%02d:%02d:%02d" % (h, m, s))
-		print "*" * 80
-		print "\n%s%s %s | %s %s" % (' ' * 10, s1, s2, s3, s4)
-		print "%sCurrent time: %s\n" % (' ' * 19, this_time)
-		print "*" * 80
-		print "%s%s\n\n%s\n%s" % (S, msg, MR, ER)
-		print "*" * 80
-		print str2red("Xtimeleft=%d" % (int(target/60)))
-	else:
-		print "\n***** No bus for you now *****\n"
+        if len(that_time) > 0:
+                s1 = str2red("Next bus time:")
+                s2 = str2yellow(that_time)
+                s3 = str2red("Time left:")
+                s4 = str2yellow("%02d:%02d:%02d" % (h, m, s))
+                print "*" * 80
+                print "\n%s%s %s | %s %s" % (' ' * 10, s1, s2, s3, s4)
+                print "%sCurrent time: %s\n" % (' ' * 19, this_time)
+                print "*" * 80
+                print "%s%s\n\n%s\n%s" % (S, msg, MR, ER)
+                print "*" * 80
+                print str2red("Xtimeleft=%d" % (int(target/60)))
+        else:
+                print "\n***** No bus for you now *****\n"
 
-	return (0)
+        return (0)
 
 def main():
-	get_minsecs(TT)
-	return (0)
+        get_minsecs(TT)
+        return (0)
 
 if __name__ == "__main__":
-	sys.exit(main())
+        sys.exit(main())
